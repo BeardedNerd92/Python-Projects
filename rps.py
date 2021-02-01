6063e76d6329531532dfebdaccf4161eb7a00a3e
Zack Grooms <intMain92@gmail.com>
2021-01-31 18:10:14 -0600

    Adeed a few more lines of code, but not done yet

--- rps.py
+++ rps.py
@@ -1,3 +1,4 @@
+
 r = "Rock"
 p = "paper"
 s = "scissors"
@@ -6,14 +7,20 @@ player = "Player"
 computer = "Computer"
 
 
-name = input(f"Hello, {player} what is your name? ").capitalize()
+name = input(f"Hello, {player} please type your name your name ").capitalize()
 game = input(f"How about a game of Rock Paper Scissors {name}? ")
 
 
 if game == "no":
     quit()
 
-elif game == "yes":
-    input(f"Okay {name} choose Rock, Paper Scissors ")
+if game == "yes" or "okay":
+    print(f"Okay I choose {r}")
+    guess1 = input(f"Your turn {name} choose Rock, Paper Scissors: ")
 
-    print(f"I choose {r}")
+    if guess1 == p:
+        print(f"You win paper covers rock.")
+    elif guess1 == r:
+        print("Tie")
+    elif guess1 == s:
+        print(f"I win rock smashes scissors.")
