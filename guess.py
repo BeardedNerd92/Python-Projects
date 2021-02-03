6b9940d41742a125bf589a33e11c43fc4d0e9d33
Zack Grooms <intMain92@gmail.com>
2021-02-02 20:45:07 -0600

    A little game of guess the correct number./guess.py

---
+++ guess.py
@@ -0,0 +1,27 @@
+import random
+
+name = input('Please enter your name: ').capitalize()
+computer = input(f'Hello {name}, I am thinking of a number between 1 and 100... (Please press enter to continue)')
+user_input = input('Do you want to try guessing the number... (Enter y/n)? ')
+
+computer_input = random.randint(70,100)
+
+def secret_num():
+    while True:
+        if user_input.lower() == 'y':
+            guess = int(input(f'Input a number {name}: '))
+        elif user_input == 'n':
+            print(f'Okay {name}, thanks for playing')
+            print('Now signing off...')
+        if guess > computer_input:
+            print(f'Sorry {name}, the number {guess} is less than the one chosen.')
+        elif guess < computer_input:
+            print(f'Sorry {name}, the number {guess} is greater than the one chosen.')
+        elif guess == computer_input:
+            print(F'NICE! {name}, {guess} is the number I am looking for.')
+
+        try_again = input(f'Would you like another shot {name} (Enter y/n): ')
+        if try_again.lower() != 'y':
+            print(f'Thanks for playing {name}.')
+            break 
+secret_num()
